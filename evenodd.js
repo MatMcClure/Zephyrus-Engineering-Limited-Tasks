3e76c769d85a5c28d7525e108fe13458334889d9
Matthew McClure <108655204+MatMcClure@users.noreply.github.com>
2024-11-20 15:40:02 -0500

    Update evenodd.js
    Added another function that asks for the user input since I was unsure if the even or odd task should have been predetermined or not.

--- evenodd.js
+++ evenodd.js
@@ -16,3 +16,26 @@ function isEvenOrOdd(num) {
 console.log(isEvenOrOdd(3)); 
 
 console.log(isEvenOrOdd(8)); 
+
+
+/*
+  The code below is similar to the top except it ask for a user input.
+  I used prompt as a built in function to display the request. I use parseInt
+  to convert the string into an integer, and in my if statement, "isNaN" is used
+  to make sure the input is an integer. If not, the user will be asked to input a 
+  valid integer. If a valid integer is entered, "Even" or "Odd" will be printed.
+/*
+
+function isEvenOrOdd() {
+  const input = prompt("Input a number:"); 
+  const num = parseInt(input);
+
+  if (isNaN(num)) {
+    console.log("Please enter a valid integer.");
+  } else {
+    console.log(num % 2 == 0 ? "Even" : "Odd");
+  }
+}
+
+isEvenOrOdd();
+*/
